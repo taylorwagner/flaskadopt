86cf7ef779cbbc60959685c3457d9e897c16ed9e
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-06-01 15:05:24 -0500

    adding wtf.validators

--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
+from wtforms.validators import InputRequired, Length, NumberRange, URL, Optional
 
 class AddNewPetForm(FlaskForm):
     """Create form for adding new pets"""
     name = StringField("Pet Name")
     species = StringField("Species")
-    photo_url = StringField("URL of Photo")
-    age = IntegerField("Age")
-    notes = StringField("Notes")
+    photo_url = StringField("URL of Photo", validators=[Optional()])
+    age = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30)])
+    notes = StringField("Notes", validators=[Optional()])

a4bd8a097c3f0bf13b9c3cbb255dc4d6a821db0b
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-06-02 11:59:10 -0500

    custom messages for validators

--- forms.py
+++ forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import InputRequired, NumberRange, URL, Optional
 
 class AddNewPetForm(FlaskForm):
     """Create form for adding new pets"""
-    name = StringField("Pet Name", validators=[InputRequired()])
-    species = RadioField("Species", choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')], validators=[InputRequired()])
+    name = StringField("Pet Name", validators=[InputRequired(message="Pet must have a name")])
+    species = RadioField("Species", choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')], validators=[InputRequired(message="Must select a species for pet")])
     photo_url = StringField("URL of Photo", validators=[Optional(), URL()])
     age = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30)])
     notes = StringField("Notes", validators=[Optional()])

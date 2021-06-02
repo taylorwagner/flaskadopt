6ee5845d5bca2354ea99093a5528917252904fb4
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-06-02 09:59:00 -0500

    url validator on form

--- forms.py
+++ forms.py
@@ -6,13 +6,13 @@ class AddNewPetForm(FlaskForm):
     """Create form for adding new pets"""
     name = StringField("Pet Name", validators=[InputRequired()])
     species = SelectField("Species", choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')], validators=[InputRequired()])
-    photo_url = StringField("URL of Photo", validators=[Optional()])
+    photo_url = StringField("URL of Photo", validators=[Optional(), URL()])
     age = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30)])
     notes = StringField("Notes", validators=[Optional()])
 
 
 class EditPetForm(FlaskForm):
     """Create form for editing pet"""
-    photo_url = StringField("URL of Photo", validators=[Optional()])
+    photo_url = StringField("URL of Photo", validators=[Optional(), URL()])
     notes = StringField("Notes", validators=[Optional()])
     available = BooleanField("Availability", validators=[InputRequired()])

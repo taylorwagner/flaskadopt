d18b91b039f25c5d6424a01e9ebf84f0d5794568
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-06-02 09:56:04 -0500

    change species to selectfield and style button to add pet

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField
+from wtforms import StringField, IntegerField, BooleanField, SelectField
 from wtforms.validators import InputRequired, Length, NumberRange, URL, Optional
 
 class AddNewPetForm(FlaskForm):
     """Create form for adding new pets"""
     name = StringField("Pet Name")
-    species = StringField("Species")
+    species = SelectField("Species", choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')])
     photo_url = StringField("URL of Photo", validators=[Optional()])
     age = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30)])
     notes = StringField("Notes", validators=[Optional()])

cc6a1f8ac4dff1b12b92924510d03a4f72b7f341
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-06-02 10:27:00 -0500

    change species field type

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField, SelectField
+from wtforms import StringField, IntegerField, BooleanField, RadioField
 from wtforms.validators import InputRequired, Length, NumberRange, URL, Optional
 
 class AddNewPetForm(FlaskForm):
     """Create form for adding new pets"""
     name = StringField("Pet Name", validators=[InputRequired()])
-    species = SelectField("Species", choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')], validators=[InputRequired()])
+    species = RadioField("Species", choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')], validators=[InputRequired()])
     photo_url = StringField("URL of Photo", validators=[Optional(), URL()])
     age = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30)])
     notes = StringField("Notes", validators=[Optional()])

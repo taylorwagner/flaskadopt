e70baec0d4f49079973671365e774751485081d1
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-06-02 07:51:39 -0500

    flask form in forms.py for editing a pet

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, BooleanField
 from wtforms.validators import InputRequired, Length, NumberRange, URL, Optional
 
 class AddNewPetForm(FlaskForm):
@@ -9,3 +9,10 @@ class AddNewPetForm(FlaskForm):
     photo_url = StringField("URL of Photo", validators=[Optional()])
     age = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30)])
     notes = StringField("Notes", validators=[Optional()])
+
+
+class EditPetForm(FlaskForm):
+    """Create form for editing pet"""
+    photo_url = StringField("URL of Photo", validators=[Optional()])
+    notes = StringField("Notes", validators=[Optional()])
+    available = BooleanField("Availability")

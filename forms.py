247ccbb84a198e099472f60c2c5cc2a1aba94852
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-06-02 20:34:41 -0500

    changes species in add-new-pet form to selectfield instead of radiofield for styling purposes

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField, RadioField
+from wtforms import StringField, IntegerField, BooleanField, SelectField
 from wtforms.validators import InputRequired, NumberRange, URL, Optional
 
 class AddNewPetForm(FlaskForm):
     """Create form for adding new pets"""
     name = StringField("Pet Name", validators=[InputRequired(message="Pet must have a name")])
-    species = RadioField("Species", choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')], validators=[InputRequired(message="Must select a species for pet")])
+    species = SelectField("Species", choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')], validators=[InputRequired(message="Must select a species for pet")])
     photo_url = StringField("URL of Photo", validators=[Optional(), URL()])
     age = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30)])
     notes = StringField("Notes", validators=[Optional()])

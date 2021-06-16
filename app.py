7dcbb7bf97798a31d95dcad36a8c5d67670d3042
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-06-16 07:39:51 -0500

    adding postgres to heroku

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///adopt')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'hellosecret1')

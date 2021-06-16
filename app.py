43f04f569015aad3eab8da622b96cc2e82eb28a6
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-06-16 07:26:51 -0500

    updating app to push to heroku

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ from flask import Flask, render_template, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
 from forms import AddNewPetForm, EditPetForm
+import os
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = "santanarush"
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'hellosecret1')
+print('************************')
+print('************************')
+print('************************')
+print(app.config["SECRET_KEY"])
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
